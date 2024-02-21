8936c7d5d511c3bf28b8053096d93f1e46e2849a
Polina <polya_vi_13@mail.ru>
2024-02-21 12:32:02 +0300

    Написали функцию нахождения необходимых строк

--- test.cs
+++ test.cs
@@ -4,3 +4,14 @@
 
 string[] array1 = new string[6] {"1234", "156", "hello", "world", "2", "Kazan"};
 string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for(int i=0; i<array1.Length; i++)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+SecondArray(array1, array2);
