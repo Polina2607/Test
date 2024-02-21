6146d4137a153ebfea46ab5073cc56d9072aa30e
Polina <polya_vi_13@mail.ru>
2024-02-21 12:36:02 +0300

    Написали функция вывода массива с необходимыми строками

--- test.cs
+++ test.cs
@@ -15,3 +15,13 @@ void SecondArray(string[] array1, string[] array2)
     }
 }
 SecondArray(array1, array2);
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+PrintArray(array2);
